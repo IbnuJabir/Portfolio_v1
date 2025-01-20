bfcf1f2f8c4fb3f09ad21167574dd0b20edb7f3b
IbnuJabir <kedirjabir12@gmail.com>
2025-01-20 08:07:00 +0300

    feat(config): add image domains and extend Tailwind CSS configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,35 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // add image domains
+  images: {
+    domains: [
+      "assets.aceternity.com",
+      "zadalmead.com",
+      "trello-on-rails.vercel.app",
+      "github.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zadalmead.com",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "trello-on-rails.vercel.app",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
