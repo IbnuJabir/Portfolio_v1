1ba05582c9ba65ef356b3bbf91120f16c6abf439
IbnuJabir <kedirjabir12@gmail.com>
2025-02-02 07:00:38 +0300

    chore(ui): add new sections and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 "use client";
-import { Moon, MoonIcon, Sun } from "lucide-react";
+import {
+  Github,
+  Instagram,
+  Linkedin,
+  Moon,
+  MoonIcon,
+  Sun,
+  X,
+} from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { InteractiveGridPattern } from "@/components/ui/interactive-grid-pattern";
@@ -15,8 +23,17 @@ import {
   IconNewSection,
   IconTerminal2,
 } from "@tabler/icons-react";
+import MorphingText from "@/components/ui/morphing-text";
+import Link from "next/link";
+import Experience from "@/components/Experience";
+import About from "@/components/About";
+import { Spotlight } from "@/components/ui/spotlight-new";
+import { Projects } from "@/components/Projects";
+import Skills from "@/components/Skills";
+
 export default function Home() {
   const { setTheme } = useTheme();
+
   const links = [
     {
       title: "Home",
@@ -54,45 +71,42 @@ export default function Home() {
     },
 
     {
-      title: "Twitter",
+      title: "Theme",
       icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-  ];
-  return (
-    <div className="w-full flex flex-col justify-between items-center justify-items-center gap-16 py-10 px-16">
-      <header className="z-10 w-full flex items-baseline justify-between align-bottom">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={100}
-          height={100}
-          className="text-blue-500 w-10 h-10"
-        />
         <Button
-          variant="outline"
+          variant="customed"
           size="icon"
           onClick={() =>
             setTheme((theme) => (theme === "dark" ? "light" : "dark"))
           }
-          className="z-50"
+          className="z-50 bg-transparent"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+          <Sun className="rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+          <Moon className="absolute rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
+      ),
+    },
+  ];
+  return (
+    <div className="w-full overflow-x-hidden flex flex-col justify-between items-center justify-items-center gap-16 py-10">
+      <header className="z-10 w-full flex items-baseline justify-center md:justify-between align-bottom md:px-16">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={100}
+            height={100}
+            className="text-blue-500 w-10 h-10"
+          />
+        </Link>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold text-center z-10">Kedir Jabir</h1>
+      {/* <Spotlight /> */}
+      <main className="w-full flex flex-col justify-between gap-8 row-start-2 items-center sm:items-start z-10">
+        <About />
+        <Experience />
+        <Projects />
+        <Skills />
       </main>
       <footer className="fixed bottom-5 left-0 right-0 row-start-3 flex gap-6 flex-wrap items-center justify-center z-10 ">
         <FloatingDock
@@ -105,7 +119,7 @@ export default function Home() {
         height={40}
         className={cn(
           "[mask-image:radial-gradient(750px_circle_at_center,white,transparent)]",
-          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
+          "inset-x-0 inset-y-[-100%] h-[200%] skew-y-12"
         )}
       />
     </div>
